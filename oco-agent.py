ab8d6c0e910db28e57756afee3123fe0ac511535
Georg Sieber <it@georg-sieber.de>
2021-07-16 22:21:11 +0200

    bump version

--- oco-agent.py
+++ oco-agent.py
@@ -34,7 +34,7 @@ import pyedid
 from zipfile import ZipFile
 
 
-AGENT_VERSION = "0.9.3"
+AGENT_VERSION = "0.9.4"
 EXECUTABLE_PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
 DEFAULT_CONFIG_PATH = EXECUTABLE_PATH+"/oco-agent.ini"
 LOCKFILE_PATH = tempfile.gettempdir()+'/oco-agent.lock'
