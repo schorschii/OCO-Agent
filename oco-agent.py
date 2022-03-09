b9814af19f9ddac5139b9a745c12902dc0ab8f06
Georg Sieber <it@georg-sieber.de>
2022-03-09 21:39:38 +0100

    bump version

--- oco-agent.py
+++ oco-agent.py
@@ -34,7 +34,7 @@ from zipfile import ZipFile
 from dns import resolver, rdatatype
 
 
-AGENT_VERSION = "0.13.0"
+AGENT_VERSION = "0.13.1"
 EXECUTABLE_PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
 DEFAULT_CONFIG_PATH = EXECUTABLE_PATH+"/oco-agent.ini"
 LOCKFILE_PATH = tempfile.gettempdir()+'/oco-agent.lock'
