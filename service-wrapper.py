0775fb3aa3d1ef02d160825a2f56624dbc9e4387
Georg Sieber <it@georg-sieber.de>
2022-09-01 14:08:28 +0200

    update copyright

--- service-wrapper.py
+++ service-wrapper.py
@@ -6,7 +6,7 @@ import subprocess, time, os, sys, signal, win32serviceutil, win32service, win32e
 class OcoWinService(win32serviceutil.ServiceFramework):
 	_svc_name_ = "oco-agent"
 	_svc_display_name_ = "OCO Agent"
-	_svc_description_ = "Open Computer Orchestration Agent for Windows (c) Georg Sieber 2021"
+	_svc_description_ = "Open Computer Orchestration Agent for Windows (c) Georg Sieber 2020-2022"
 
 	runflag = True
 	process = None
