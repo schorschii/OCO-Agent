1a3a9865309e9a76509ab006ed7c06aa8c1f8460
Georg Sieber <it@georg-sieber.de>
2025-04-07 15:44:05 +0200

    bump version

--- installer/macos/build.sh
+++ installer/macos/build.sh
@@ -5,7 +5,7 @@ set -e
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 TARGET_DIRECTORY="$SCRIPTPATH/target"
 PRODUCT="OCO Agent"
-VERSION="1.1.8"
+VERSION="1.1.9"
 TARGET_FILENAME="oco-agent.pkg"
 #DEVELOPER_ID_INSTALLER_CERT_NAME=""
 #DEVELOPER_ACCOUNT_USERNAME=""
