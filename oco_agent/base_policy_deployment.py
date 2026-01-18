9eec62dd22479c79b6e432e873007eb5ad538ab3
Georg Sieber <it@georg-sieber.de>
2026-01-18 16:34:52 +0100

    support for nested JSON values (Firefox)

--- oco_agent/base_policy_deployment.py
+++ oco_agent/base_policy_deployment.py
@@ -37,21 +37,32 @@ class BasePolicyDeployment:
 
 	def applyJsonPolicy(self, scope, path, key, value):
 		# load existing file if exists
-		values = {}
+		jsonStruct = {}
 		if(os.path.isfile(path)):
 			with open(path, 'r') as file:
-				values = json.load(file)
+				jsonStruct = json.load(file)
 
-		# apply new values
-		# append as dict/array if string can be parsed as JSON
+		# append value as dict/array if string can be parsed as JSON
 		if(isinstance(value, str)):
 			try:
 				parsed = json.loads(value)
 				value = parsed
 			except json.decoder.JSONDecodeError: pass
-		values[key] = value
+
+		# apply new values atomic - keep existing (sub)keys/values
+		keyPath = key.split('\\')
+		value = self.insertValueInStruct(jsonStruct, keyPath, value)
 
 		# write updated policies file
 		os.makedirs(os.path.dirname(path), exist_ok=True)
 		with open(path, 'w') as file:
-			json.dump(values, file, indent=2)
+			json.dump(jsonStruct, file, indent=2)
+
+	def insertValueInStruct(self, struct, keyPath, value):
+		if(len(keyPath) == 1):
+			struct[keyPath[0]] = value
+		else:
+			subStruct = {}
+			if(keyPath[0] in struct): subStruct = struct[keyPath[0]]
+			struct[keyPath[0]] = self.insertValueInStruct(subStruct, keyPath[1:], value)
+		return struct
